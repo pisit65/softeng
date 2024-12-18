13a94c8a0fcd0a1bb10fe83c9aa94f70ea604ab0
pisit65 <pisit.ph.65@ubu.ac.th>
2024-12-18 13:05:26 +0700

    'page1'

---
+++ OneDrive/เดสก์ท็อป/softENG/mypro/myapp/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+def page2(request):
+    return render(request, 'page2.html')
